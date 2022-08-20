a939ae0bf9ec2c641be9077c164b893a0f69d6de
Eduardo <educonts08@gmail.com>
2022-08-20 15:06:13 -0300

    useCharacter from hooks created and function  handleKeyDown

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,43 @@
+import { useEffect } from 'react'
 import { Container, Map } from './styles/App.styles';
 import { Character } from './components/Character'
+import { useCharacter } from './hooks/useCharacter'
 
 const App = () => {
+  const char = useCharacter()
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown)
+  }, [])
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    switch(e.code){
+      case 'KeyA':
+      case 'ArrowLeft':
+        char.moveLeft()
+      break;
+
+      case 'KeyW':
+      case 'ArrowUp':
+        char.moveUp()
+      break;
+
+      case 'KeyD':
+      case 'ArrowRight':
+        char.moveRight()
+      break;
+
+      case 'KeyS':
+      case 'ArrowDown':
+        char.moveDown()
+      break;
+    }
+  }
+
   return(
     <Container>
       <Map>
-        <Character x={5} y={0}/>
+        <Character x={char.x} y={char.y} />
       </Map>
     </Container>
   )
