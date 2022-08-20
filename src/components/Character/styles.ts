81c9689f4f136a387bcb40139a7033553fb90d2d
Eduardo <educonts08@gmail.com>
2022-08-20 15:16:07 -0300

    Created CharacterSides.ts from types

--- src/components/Character/styles.ts
+++ src/components/Character/styles.ts
@@ -6,6 +6,6 @@ export const Container = styled.div<{ size: number, left: number, top: number }>
   position: absolute;
   left: ${props => props.left}px;
   top: ${props => props.top}px;
-  
-  background-color: red;
+
+  background-image: url('/assets/char.png')
 `;
