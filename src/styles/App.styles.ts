9ab2282fc40a31175c329ac6e20d98c457306cde
Eduardo <educonts08@gmail.com>
2022-08-19 22:53:08 -0300

    map component updated

--- src/styles/App.styles.ts
+++ src/styles/App.styles.ts
@@ -4,4 +4,16 @@ export const Container = styled.div`
   background-color: #24282F;
   min-height: 100vh;
   color: #fff;
+  overflow-y: auto;
+`;
+
+export const Map = styled.div`
+  width: 480px;
+  height: 480px;
+
+  background-image: url('/assets/map.png');
+  background-position: center;
+  margin: auto;
+  margin-top: 50px;
+  background-size: 100%;
 `;
